512c0fb6795f9d94e8e0792e8c677347e83bec41
eid iepoh <tornike.dzmanashvili@gmail.com>
2023-04-10 16:45:01 +0400

    adds deleted underline functionality

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -9,26 +9,31 @@ const Navbar = () => {
                 <li className="navigation-list-item">
                     <NavLink exact to="/">
                         home
+                        <div className="navigation-list-underline"></div>
                     </NavLink>
                 </li>
                 <li className="navigation-list-item">
                     <NavLink to="/services">
                         services
+                        <div className="navigation-list-underline"></div>
                     </NavLink>
                 </li>
                 <li className="navigation-list-item">
                     <NavLink to="/project">
                         projects
+                        <div className="navigation-list-underline"></div>
                     </NavLink>
                 </li>
                 <li className="navigation-list-item">
                     <NavLink to="/about">
                         about
+                        <div className="navigation-list-underline"></div>
                     </NavLink>
                 </li>
                 <li className="navigation-list-item">
                     <NavLink to="/contact">
                         contact
+                        <div className="navigation-list-underline"></div>
                     </NavLink>
                 </li>
             </ul>
